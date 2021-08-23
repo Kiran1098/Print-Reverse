7a4f353d93383e599898128ea4bfb1e34b047b0f
user <user@DESKTOP-CNQ16QF>
2021-08-23 21:47:58 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp_Learn_Programming_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            string input  = "Hellow World";
+            
+
+            Console.WriteLine(input.Substring(input.Length-3,3));
+            
+
+        }
+    }
+}
